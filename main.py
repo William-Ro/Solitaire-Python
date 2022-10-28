a09c6f70c81f2c8f8a85d44945c0d90d133e609c
William-Ro <william.rodriguez.rocha@est.una.ac.cr>
2022-10-27 20:53:11 -0600

    We define all the sizes for the cards and create the list for all the cards we have

--- main.py
+++ main.py
@@ -2,20 +2,66 @@
 
 import arcade
 
+# Aca declaramos todas las constantes
+
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
+SCREEN_TITLE = "Solitaire"
+
+CARD_SCALE = 0.6
+CARD_WIDTH = 140 * CARD_SCALE
+CARD_HEIGHT = 190 * CARD_SCALE  # Tamanno de las cartas
+
+MAT_PERCENT_OVERSIZE = 1.25
+MAT_HEIGHT = int(CARD_HEIGHT * MAT_PERCENT_OVERSIZE)  # Tamanno de la mesa donde repartimos las cartas
+MAT_WIDTH = int(CARD_WIDTH * MAT_PERCENT_OVERSIZE)
+
+VERTICAL_MARGIN_PERCENT = 0.10  # Estos son los margenes
+HORIZONTAL_MARGIN_PERCENT = 0.10
+
+BOTTOM_Y = MAT_HEIGHT / 2 + MAT_HEIGHT * VERTICAL_MARGIN_PERCENT
+START_X = MAT_WIDTH / 2 + MAT_WIDTH * HORIZONTAL_MARGIN_PERCENT
+
+CARD_VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]  # Declaramos las cartas
+CARD_SUITS = ["Clubs", "Hearts", "Spades", "Diamonds"]
+
+
+class card(arcade.Sprite):
+
+    def __init__(self, sign, value, scale=1):
+        self.sign = sign
+
+        self.value = value
+
+        # Aca se utiliza un png de carta para cuando está volteada
+        self.image_file_name = f":resources:images/cards/card{self.sign}{self.value}.png"
+        super().__init__(self.image_file_name, scale, hit_box_algorithm="None")
+
 
 class solitaire(arcade.Window):  # Aca declaramos nuestra clase juego
 
     def __init__(self):  # Constructor donde declaramos el tamanno de la ventana
 
-        super().__init__(1024, 768, "Solitaire")
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        self.card_list = None  # Creamos una lista donde van a ir metidas todas las cartas, el llamado "mazo"
 
         arcade.set_background_color(arcade.color.AMAZON)  # Hacemos la ventana de color verde
 
-    def setup(self):
-        pass  # Esto nos sirve para reiniciar el juego
+    def setup(self):  # Esta funcion nos sirve para reiniciar el juego
 
-    def clear_screen(self):
+        self.card_list = arcade.SpriteList()
+        for card_sign in CARD_SUITS:
+            for card_value in CARD_VALUES:
+                card_aux = card(card_sign, card_value, CARD_SCALE)
+                card_aux.position = START_X, BOTTOM_Y  # Esto es para llenar nuestra lista con cada una de las cartas
+                self.card_list.append(card_aux)  # nuestras cartas son los "sprite"
+
+    def on_draw(self):
+        # Draw the cards
         self.clear()
+
+        self.card_list.draw()
 
     def on_mouse_press(self, x, y, button, key_modifiers):
         # Esto es para cada vez que un usuario da click nos devuelve la posicion
@@ -31,7 +77,6 @@ class solitaire(arcade.Window):  # Aca declaramos nuestra clase juego
 
 
 def main():
-
     window = solitaire()  # Declaramos nuestro main y hacemos los respectivos llamados
     window.setup()
     arcade.run()
